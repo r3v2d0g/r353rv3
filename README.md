ca7bf8ffff02ab664128f85773e5e1ef08350863
Matthieu Le brazidec <matthieu@lebrazidec.email>
2019-12-15 05:47:21 +0100

    Removed the instructions, added copyright notices, fixed the default author and added more variables.

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # reserve.sh
 
-A simple script to reserve [crates](https://crates.io/).
+## Copyright
 
-## Usage
+```
+Copyright (c) 2019
+All Rights Reserved to Matthieu Le brazidec
+Unauthorized copying of any file in this project, via any medium is strictly prohibited
+Proprietary and confidential
+```
 
-1) Edit the `reserve.sh` file to use your name (`$AUTHORS`) and an URL of your choice (`$HOMEPAGE`).
-2) Run `cargo login` (after creating an account on [https://crate.io/](https://crates.io) if you don't already have one).
-3) Run `./reserve.sh [CRATES...]` with `CRATES` as the list of crates you want to reserve (separated by a whitespace).
-
-**NOTE**: the newly created crates will be published as being under the MIT license (you can modify this in the `reserve.sh` file).
-
-**NOTE**: a new directory will be created for each crate. You can safely remove it.
--- reserve.sh
+++ reserve.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 
-AUTHORS='["Matthieu Le brazidec <matthieu@lebrazidec.email"]'
+############################################################################
+# Copyright (c) 2019                                                       #
+# All Rights Reserved to Matthieu Le brazidec                              #
+# Unauthorized copying of this file, via any medium is strictly prohibited #
+# Proprietary and confidential                                             #
+############################################################################
+
+DESCRIPTION="Reserved for future use."
+LICENSE="MIT"
+AUTHORS='["Matthieu Le brazidec <matthieu@lebrazidec.email>"]'
 HOMEPAGE="https://github.com/azastrael"
 
 base=$(pwd)
+
+echo ''
+echo '###########################################################################################'
+echo '# Copyright (c) 2019                                                                      #'
+echo '# All Rights Reserved to Matthieu Le brazidec                                             #'
+echo '# Unauthorized copying of any file in this project, via any medium is strictly prohibited #'
+echo '# Proprietary and confidential                                                            #'
+echo '###########################################################################################'
+echo ''
 
 for pkg in $*; do
 	mkdir -p $pkg/src
@@ -15,12 +33,13 @@ for pkg in $*; do
 	echo '[package]' >> Cargo.toml
 	echo "name        = \"$pkg\"" >> Cargo.toml
 	echo 'version     = "0.0.0"' >> Cargo.toml
-	echo 'description = "Reserved for future use."' >> Cargo.toml
-	echo 'license     = "MIT"' >> Cargo.toml
+	echo "description = \"$DESCRIPTION\"" >> Cargo.toml
+	echo "license     = \"MIT\"" >> Cargo.toml
 	echo "authors     = $AUTHORS" >> Cargo.toml
 	echo "homepage    = \"$HOMEPAGE\"" >> Cargo.toml
 
 	cargo publish --allow-dirty
 
 	cd $base
+	rm -r $pkg
 done
